50612f430e553ec413ea6eba761ec8ddd7d984e5
artemplv <artemplv@gmail.com>
2023-05-23 15:17:33 -0400

    add move piece

--- practice-for-ch-ruby-chess-long-practice-main/Board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/Board.rb
@@ -1,33 +1,70 @@
 require_relative "Piece"
 
 class Board
-
   def initialize
     @rows = Array.new(8) { Array.new(8) {Piece.new("null")}}
+    self.fillBoard
+  end
+
+  def inspect
+    "id: #{self.object_id}, rows: #{@rows}"
+  end
+
+  def printBoard
+    @rows.each do |row|
+        row.each do |cell|
+            print [cell.type]
+        end
+        print "\n"
+    end
+    nil
+  end
+        
+  def move_piece(start_pos, end_pos)
+    if !valid_pos?(start_pos)
+        raise "Not a valid starting position"
+    elsif self[start_pos].type == 'null'
+        raise "There is no piece at this position"
+    elsif !valid_pos?(end_pos)
+        raise "Piece cannot move to end position"
+    end
+  end
+
+  def valid_pos?(pos)
+    x, y = pos
+
+    return false if x > 7 || x < 0 || y > 7 || y < 0
+    true
+  end
+
+  def [](pos)
+    x, y = pos
+    @rows[x][y]
   end
 
   private
-  def fillBoard
+  def fillBoard    
     [1,6].each do |idx|
-        @rows[idx].each do |cell|
-            cell = Piece.new("pawn")
+        (0..7).each do |cellIdx|
+            @rows[idx][cellIdx] = Piece.new("pawn")
         end 
     end 
+    
     [0,7].each do |idx|
-        @rows[idx].each_with_index do |cell, cellIdx|
-        case cellIdx 
-        when 0 || 7 
-            cell = Piece.new("rook")
-        when 1 || 6 
-            cell = Piece.new("knight")
-        when 2 || 5 
-            cell = Piece.new("bishop")
-        when 3 
-            cell = Piece.new("queen")
-        when 4
-            cell = Piece.new("king")
-        end 
+        (0..7).each do |cellIdx|
+            if cellIdx == 0 || cellIdx == 7
+                @rows[idx][cellIdx] = Piece.new("rook")
+            elsif cellIdx == 1 || cellIdx == 6
+                @rows[idx][cellIdx] = Piece.new("knight")
+            elsif cellIdx == 2 || cellIdx == 5
+                @rows[idx][cellIdx] = Piece.new("bishop")
+            elsif cellIdx == 3
+                @rows[idx][cellIdx] = Piece.new("queen")
+            elsif cellIdx == 4 
+                @rows[idx][cellIdx] = Piece.new("king")
+            end
+        end
     end 
-  end 
+  end
   
 end
