f65d963775cde25e9dfdafbf0631b219395a249a
Luckyleck <luckyleck29@gmail.com>
2023-05-23 12:16:24 -0400

    started class inheritence

---
+++ Class Inheritence.rb
@@ -0,0 +1,5 @@
+require_relative "Employee"
+require_relative "Manager"
+
+david = Employee.new("david", "TA", 10000, "darren")
+shawna = Employee.new("shawna", "TA", 12000, "darren")
---
+++ Manager.rb
@@ -0,0 +1,11 @@
+class Manager < Employee
+  def initialize(name, title, salary, boss, employees)
+    super(name, title, salary, boss)
+    @employees = employees
+  end
+
+  def bonus(multiplier)
+    total_salary = employees.inject(0) { |acc, employee| acc + employee.salary }
+    total_salary * multiplier
+  end
+end
